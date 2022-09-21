4773dc4b7cb75ad240df2bf5d533ea2f77ad5bc6
Bryce Anthony <branthony@davidson.edu>
2022-09-21 08:34:20 -0400

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,10 @@
 
 /* javascript */
+// $("h1").hide();
+// callback funciton goes in click
+$(".submit").click(function(e){
+  e.preventDefault();
+  username = $(".username").val();
+  console.log(e);
+  console.log("username:" +username)
+});
