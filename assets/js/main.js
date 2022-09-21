b9593d56c2a8e0635bdfc1e386990385cf2dd652
Bryce Anthony <branthony@davidson.edu>
2022-09-21 09:01:52 -0400

    lecture 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,25 @@
 /* javascript */
 // $("h1").hide();
 // callback funciton goes in click
-$(".submit").click(function(e){
+
+let username ="";
+let requiredUsername = "secret";
+
+$("form").submit(function(e){
   e.preventDefault();
   username = $(".username").val();
-  console.log(e);
-  console.log("username:" +username)
+  console.log(username);
+
+
+  let str = "";
+  if (requiredUsername == username){
+    console.log("welcome secret agent");
+    str =`
+    <div class="alert alert-secondary" role="alert">
+      A simple secondary alert—check it out!
+    </div>`;
+}
+
+$('output').html(str);
+
 });
